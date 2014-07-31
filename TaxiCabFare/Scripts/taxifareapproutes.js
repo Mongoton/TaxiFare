9258c4c045d489035760f516a331cf7d1a11b8c4
Mongo <benning25@gmail.com>
2014-07-30 22:09:13 -0400

    Revert "Initial Commit"
    This reverts commit b0ffb9e688db23825461def6802b1a8c9347bc7d.

--- TaxiCabFare/Scripts/taxifareapproutes.js
+++
@@ -1,5 +0,0 @@
-﻿var app = angular.module("taxifarecalc", ['ngRoute'])
-.config(function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/Home/Index', { templateUrl: '/ngViews/mainpage.html', controller: 'MainController' });
-    $locationProvider.html5Mode(true);
-});
